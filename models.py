01edb16e98c78203e0aa92ae01920a0e7585109c
opple <156044992+dedaow@users.noreply.github.com>
2024-10-30 19:03:07 +0800

    Update models.py

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ class GCN(nn.Module):
 
         self.gc1 = GraphConvolution(nfeat, nhid1)
 
-        # self.gc2 = GraphConvolution(nhid1, nhid2)
-        # self.gc3 = GraphConvolution(nhid2, nhid2)
-        # self.gc4 = GraphConvolution(nhid2, nhid2)
+        self.gc2 = GraphConvolution(nhid1, nhid2)
+        self.gc3 = GraphConvolution(nhid2, nhid2)
+        self.gc4 = GraphConvolution(nhid2, nhid2)
 
         self.gc5 = GraphConvolution(nhid1, nout)
         self.dropout = dropout
@@ -20,12 +20,12 @@ class GCN(nn.Module):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
 
-        # x = F.relu(self.gc2(x, adj))
-        # x = F.dropout(x, self.dropout, training=self.training)
-        # x = F.relu(self.gc3(x, adj))
-        # x = F.dropout(x, self.dropout, training=self.training)
-        # x = F.relu(self.gc4(x, adj))
-        # x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc2(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc3(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc4(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
 
         x = self.gc5(x, adj)
         return x
